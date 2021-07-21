c2b8519b472f833f03c77d671fb081c73c50c450
TimoBechtel <TimoBechtel@users.noreply.github.com>
2021-07-21 22:48:33 +0000

    deploy: b298780cb511853b51a0bac038828a4d86785a4e

--- dist/scrollListener.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type ScrollListener = (event: Event) => void;
-export declare const scrollListener: {
-    add(callback: ScrollListener): void;
-    remove(callback: ScrollListener): void;
-};
